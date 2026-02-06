2b9d8fd5adf50e8b993180e083d06aac3fa188bb
Rodrigo <rbustosmolini@gmail.com>
2026-02-05 22:49:16 -0300

    Added a third microcode byte, a frame pointer for fn flow.

--- asm/os.asm
+++ asm/os.asm
@@ -1,12 +1,19 @@
 init:
-    pshb #0x9c
-    pshb #0xdf
-    jsr #fnMultiply8
+    ;pshb #32
+    ;pshb #54
+    ;jsr #fnMultiply8
     
-    jsr #fnPrintHex
-    dec SP
-    jsr #fnPrintHex
     
+    ;jsr #fnPrintHex
+    ;add SP#1
+    ;jsr #fnPrintHex
+    pshw #0x4000
+    jsr #fnReadTillEnter
+    
+    pshw #0x4000
+    jsr #fnToDec
+
+    add SP#2
     hlt
     
     jmp #end
@@ -15,168 +22,49 @@ init:
     .string " - hola - ",0x0
 
 fnPrintStr:
-    decw SP
+    mov SP,FP
+    add FP#2
 
 .loop:
-    ld A[&SP]
+    ld A[&FP]
     cmp A#0x0
     jpz #.return
     st A&0x2000
-    ld A[SP]
+    ld A[FP]
     add A#1
-    st A[SP]
+    st A[FP]
 
-    dec SP
+    add FP#1
     
     ld A[SP]
     adc A#0
     st A[SP]
 
-    inc SP
+    sub FP#1
+    
     jmp #.loop
 .return:
-    incw SP
+    
     rts
 
-
-end:
-    jmp #end
-
-fnAdd16:
-    decw SP
-    
-    ld A[SP]
-    decw SP
-    ld B[SP]
-    add A
-    incw SP
-    st A[SP]
-
-    dec SP
-    
-    ld A[SP]
-    decw SP
-    ld B[SP]
-    add A
-    incw SP
-    st A[SP]
-   
-
-    inc SP
-
-    ld A[SP]
-    adc A#0x0
-    st A[SP]
-
-
-    incw SP
-
-    rts
-
-fnShiftLeft16:
-    decw SP
-    
-    ld A[SP]
-    shl 
-    st A[SP]
-
-    
-    dec SP
-    ld A[SP]
-    shl 
-    st A[SP]
-
-    inc SP
-
-    ld A[SP]
-    adc A#0x0
-    st A[SP]
-
-    
-    
-    incw SP
-
-    rts
-
-fnMultiply8:
-    ;pshb #0x0 ;spf + 1 shiftRef
-    ;pshw #0x0 ;spf + 3 shiftAdd
-    ;pshw #0x0 ;spf + 5 res
-    
-    ;decd SP   ;spf + 1
-    ;dec SP    ;spf + 0
-    decw SP   ;spf - 2 [arg 1]
-    
-    ld A[SP]
-    dec SP    ;spf - 3 [arg 0]
-    ld B[SP]
-
-    incd SP    ;spf + 1 shiftRef
-    
-    st B[SP]
-    inc SP    ;spf + 2 shiftAdd l
-    st A[SP]
-    dec SP    ;spf + 1 shiftRef 
-    
+fnPrintInvStr:
+    mov SP,FP
+    sub FP#1
 .loop:
-    ld A[SP]
-    and A#0x1
-    incd SP   ;spf + 5 res
-    jnz #.add 
-.goback:    
-    decd SP   ;spf + 1 shiftRef
-    ld A[SP]
-    shr
-    st A[SP]
-    
+    ld A[FP]
     cmp A#0x0
     jpz #.return
-
-    incw SP   ;spf + 3 shiftAdd
-
-    ld A[SP]
-    shl 
-    st A[SP]
-    
-    dec SP
-    ld A[SP]
-    shl 
-    st A[SP]
-
-    inc SP
-
-    ld A[SP]
-    adc A#0x0
-    st A[SP]
-
-    decw SP   ;spf + 1 shiftRef 
-
+    st A&0x2000
+    sub FP#1
     jmp #.loop
-.add:
-    jsr #fnAdd16
-    jmp #.goback
 .return:
-    incd SP   ;spf + 5 res
-    ld A[SP]
-    dec SP    ;spf + 4 res l
-    ld B[SP]
-    decd SP   ;spf + 0 
-    decw SP   ;spf - 2 [arg 1]
-    st A[SP]
-    dec SP 
-    st B[SP]  ;spf - 3 [arg 0]
     
-    inc SP
-    incw SP
-
     rts
 
 
 fnPrintHex:
-    decw SP
-    ld A[SP]
-    incw SP
-    psh A
+    add FP,SP#2
+    ld A[FP]
     
     shr4
     cmp A#0xa
@@ -184,19 +72,188 @@ fnPrintHex:
     add A#'0'
     st A&0x2000
     
-    pop A
+    ld A[FP]
     and A#0xf
     cmp A#0xa
     bcs #.letter
     add A#'0'
     st A&0x2000
     
+    ;mov SP,FP
     rts
 
 .letter:
     add A#0x7
     rts
 
-fnToDec:
+
+
+end:
+    jmp #end
+
+fnAdd16:
+    add FP,SP#2
+
+    ld A[FP]
+    add FP,SP#4
+    ld B[FP]
+    add A
+    add FP,SP#2
+    st A[FP]
+
+    add FP,SP#3
+    
+    ld A[FP]
+    add FP,SP#5
+    ld B[FP]
+    add A
+    add FP,SP#3
+    st A[FP]
+   
+
+    add FP,SP#2
+
+    ld A[FP]
+    adc A#0x0
+    st A[FP]
+
     rts
 
+fnShiftLeft16:
+    add FP,SP#2
+    
+    ld A[FP]
+    shl 
+    st A[FP]
+
+    add FP,SP#3
+    
+    ld A[FP]
+    shl 
+    st A[FP]
+
+    add FP,SP#2
+
+    ld A[FP]
+    adc A#0x0
+    st A[FP]
+
+    rts
+
+fnMultiply8:
+    sub SP#5
+    ; 0.1 res   2.3 add   4 ref   5.6 PC   7 arg1   8 arg0
+    add FP,SP#7
+    
+    ld A[FP]
+    add FP,SP#8
+    ld B[FP]
+
+    add FP,SP#4
+    
+    st B[FP]
+    add FP,SP#3
+    st A[FP]
+    add FP,SP#4
+    
+.loop:
+    ld A[FP]
+    and A#0x1
+    jnz #.add
+.goback:
+    add FP,SP#4
+
+    ld A[FP]
+    shr
+    st A[FP]
+    
+    cmp A#0x0
+    jpz #.return
+
+    add FP,SP#2
+    ld A[FP]
+    add FP,SP#3
+    ld b[FP]
+
+    psh B
+    psh A
+    jsr #fnShiftLeft16
+    pop A 
+    pop B
+
+    add FP,SP#2
+    st A[FP]
+    add FP,SP#3
+    st b[FP]
+    
+    add FP,SP#4
+    jmp #.loop
+.add:
+    
+    jsr #fnAdd16
+    jmp #.goback
+.return:
+    
+    mov SP,FP
+    ld A[FP]
+    add FP#1
+    ld B[FP]
+
+    add FP,SP#7
+    st A[FP]
+    add FP,SP#8
+    st B[FP]
+
+    add SP#5
+
+    rts
+
+
+
+fnToDec:
+    sub 
+    add FP,SP#4
+.loop:
+    jmp #.loop
+.return:
+    rts
+
+fnReadTillEnter:
+    add FP,SP#2
+.loop:
+    ld A&0x2000
+    cmp A#0x0
+    jnz #.put
+    jmp #.loop
+.put:
+    cmp A#0xd
+    jpz #.return
+    st A[&FP]
+    
+    ld A[FP]
+    add FP,SP#3
+    ld B[FP]
+    
+    pshsw #1
+    psh A
+    psh B
+    jsr #fnAdd16
+    pop B
+    pop A
+    add SP#2
+
+    add FP,SP#2
+    st A[FP]
+    add FP,SP#3
+    st B[FP]
+
+    add FP,SP#2
+
+    jmp #.loop
+.return:
+    
+    rts
+
+fnNormalizeString:
+    
+    rts
--- src/emulator.cpp
+++ src/emulator.cpp
@@ -2,45 +2,52 @@
 #include <fstream>
 #include <conio.h>
 #include <iomanip>
+/*
+#define DEBUG0
+#define DEBUG1
+#define DEBUG2
+*/
+#define NOFLG 0b001000000000000000000000
+#define XI    0b000100000000000000000000
+#define XO    0b000010000000000000000000
+#define FPI   0b000001000000000000000000
+#define SPI   0b000000100000000000000000
+#define CI    0b000000010000000000000000
 
-//#define DEBUG
+#define SPNOT 0b000000001000000000000000  // 0
+#define ME    0b000000000100000000000000  // 1
+#define MI    0b000000000010000000000000  // 1
+#define RI    0b000000000001000000000000  // 1
+#define II    0b000000000000100000000000  // 1
+#define PE    0b000000000000010000000000  // 1
+#define PI    0b000000000000001000000000  // 1
+#define RS    0b000000000000000100000000  // 1
 
-
-#define SPNOT 0b1000000000000000 // 0
-#define ME 0b0100000000000000    // 1
-#define MI 0b0010000000000000    // 1
-#define RI 0b0001000000000000    // 1
-#define II 0b0000100000000000    // 1
-#define PE 0b0000010000000000    // 1
-#define PI 0b0000001000000000    // 1
-#define RS 0b0000000100000000    // 1
-
-#define A3 0b0000000010000000  // 1
-#define A2 0b0000000001000000  // 1
-#define A1 0b0000000000100000  // 0
-#define A0 0b0000000000010000  // 1
-#define HI 0b0000000000001000  // 1
-#define SPI 0b0000000000000100 // 1
-#define BI 0b0000000000000010  // 1
-#define AI 0b0000000000000001  // 0
+#define A3    0b000000000000000010000000 // 1
+#define A2    0b000000000000000001000000 // 1
+#define A1    0b000000000000000000100000 // 0
+#define A0    0b000000000000000000010000 // 1
+#define HI    0b000000000000000000001000 // 1
+#define SPACT 0b000000000000000000000100 // 1
+#define BI    0b000000000000000000000010 // 1
+#define AI    0b000000000000000000000001 // 0
 
 #define ADD A0
 #define AND A1
-#define OR A0 | A1
+#define OR A0|A1
 #define XOR A2
-#define SUB A0 | A2
-#define SPO A1 | A2
-#define BO A0 | A1 | A2
+#define SUB A0|A2
+#define SPO A1|A2
+#define BO A0|A1|A2
 #define AO A3
-#define RO A0 | A3
-#define PO A1 | A3
-#define ADC A0 | A1 | A3
-#define CO A2 | A3
-#define SHL A0 | A2 | A3
-#define SHR A1 | A2 | A3
-#define IGB A0 | A1 | A2 | A3
+#define RO A0|A3
+#define PO A1|A3
+#define ADC A0|A1|A3
+#define CO A2|A3
+#define SHL A0|A2|A3
+#define SHR A1|A2|A3
+#define FPO A0|A1|A2|A3
 
-#define CI SPNOT
 
 #define PIH PI | HI
 #define PIL PI
@@ -52,8 +59,8 @@
 struct MicrocodeSignalStruct
 {
         std::string name;
-        unsigned short signal;
-        unsigned short notsignal;
+        unsigned int signal;
+        unsigned int notsignal;
 };
 typedef unsigned char Byte;
 typedef unsigned short Word;
@@ -61,8 +68,8 @@ typedef unsigned short Word;
 struct cpuState
 {
         Word Bus;
-        Byte A, B, C;
-        Word PC, SP, BP, IR, MR;
+        Byte A, B, C, X;
+        Word PC, SP, FP, IR, MR;
         Byte MicroStep;
         Byte ZeroF, CarryF;
         Byte RAM[65536];
@@ -71,13 +78,15 @@ cpuState CPUSTATE = {0, 0, 0, 0, 0};
 
 MicrocodeSignalStruct Signals[] = {
     {"HI", HI},
-    {"SPNOT", SPNOT | SPI},
+    {"SPNOT", SPNOT},
     {"ME", ME},
     {"RI", RI},
     {"II", II},
     {"PE", PE},
     {"RS", RS},
-    {"SPI", SPI},
+    {"SPI",SPI},
+    {"FPI",FPI},
+    {"SPACT", SPACT},
     {"AI", AI},
     {"BI", BI},
     {"ADD", ADD, A1 | A2 | A3},
@@ -91,21 +100,24 @@ MicrocodeSignalStruct Signals[] = {
     {"RO", RO, A1 | A2},
     {"ADC", ADC, A2},
     {"CO", CO, A0 | A1},
-    {"CI", SPNOT, SPI},
+    {"CI", CI},
     {"SHL", SHL, A1},
     {"SHR", SHR, A0},
-    {"IGB", IGB},
+    {"FPO", FPO},
     {"PIH", PI | HI},
     {"PIL", PI, HI},
-    {"POH", PO | HI},
-    {"POL", PO, HI},
+    {"POH", PO | HI,A0|A2},
+    {"POL", PO, HI|A0|A2},
     {"MIH", MI | HI},
     {"MIL", MI, HI},
+    {"XI", XI},
+    {"XO", XO},
+    {"NOFLG",NOFLG},
 };
 
-unsigned short mCode[16384];
+unsigned int mCode[16384];
 
-void showMicro(unsigned short signals)
+void showMicro(unsigned int signals)
 {
         std::cout << "( ";
         for (auto sig : Signals)
@@ -120,7 +132,7 @@ void showMicro(unsigned short signals)
         std::cout << ")";
 }
 
-bool isSignal(std::string name, unsigned short signals)
+bool isSignal(std::string name, unsigned int signals)
 {
         for (auto sig : Signals)
         {
@@ -138,7 +150,8 @@ bool isSignal(std::string name, unsigned short signals)
 
 void sendByte(Byte data) { std::cout << data; }
 Byte recvByte()
-{
+{       
+/*#ifndef DEBUG2
         if (kbhit())
         {
                 char a = getch();
@@ -147,11 +160,17 @@ Byte recvByte()
                 return a;
         }
         return 0;
+#else*/ 
+            char a=getch();
+            return a;
+/*#endif*/
 }
 
-void executeMicroRising(Word signal)
+bool enableFlags=true;
+
+void executeMicroRising(unsigned int signal)
 {
-        if (isSignal("SPI", signal))
+        if (isSignal("SPACT", signal))
         {
                 CPUSTATE.SP -= 1;
                 if (isSignal("SPNOT", signal))
@@ -160,8 +179,20 @@ void executeMicroRising(Word signal)
                 }
                 CPUSTATE.SP = CPUSTATE.SP | 0xff00;
         }
+        if (isSignal("SPI", signal)){
+
+                CPUSTATE.SP = CPUSTATE.Bus;
+                CPUSTATE.SP = CPUSTATE.SP | 0xff00;
+        }
+        if (isSignal("FPI", signal)){
+
+                CPUSTATE.FP = CPUSTATE.Bus;
+                CPUSTATE.FP = CPUSTATE.FP | 0xff00;
+        }
         if (isSignal("AI", signal))
                 CPUSTATE.A = CPUSTATE.Bus;
+        if (isSignal("XI", signal))
+                CPUSTATE.X = CPUSTATE.Bus;
         if (isSignal("BI", signal))
                 CPUSTATE.B = CPUSTATE.Bus;
         if (isSignal("CI", signal))
@@ -172,10 +203,6 @@ void executeMicroRising(Word signal)
                 if (CPUSTATE.MR == 0x2000)
                 {
                         sendByte(CPUSTATE.Bus);
-                }
-                else if (CPUSTATE.MR == 0x2001)
-                {
-                        CPUSTATE.BP = CPUSTATE.Bus;
                 }
 
         }
@@ -201,12 +228,14 @@ void executeMicroRising(Word signal)
 
 void setFlags()
 {
+        if(enableFlags){
         CPUSTATE.ZeroF = ((CPUSTATE.Bus & 0xFF) == 0);
-        CPUSTATE.CarryF = ((CPUSTATE.Bus & 0x100) == 0x100);
+        CPUSTATE.CarryF = ((CPUSTATE.Bus & 0x100) == 0x100);}
         CPUSTATE.Bus = CPUSTATE.Bus & 0xFF;
+        enableFlags=true;
 }
 
-void executeMicroFalling(Word signal)
+void executeMicroFalling(unsigned int signal)
 {
         CPUSTATE.Bus = 0;
         if (isSignal("SPO", signal))
@@ -215,12 +244,20 @@ void executeMicroFalling(Word signal)
                 if (isSignal("HI", signal))
                         CPUSTATE.Bus = ((CPUSTATE.SP >> 8) & 0xff);
         }
+        if (isSignal("FPO", signal))
+        {
+                CPUSTATE.Bus = (CPUSTATE.FP & 0xff);
+                if (isSignal("HI", signal))
+                        CPUSTATE.Bus = ((CPUSTATE.FP >> 8) & 0xff);
+        }
         if (isSignal("AO", signal))
                 CPUSTATE.Bus = CPUSTATE.A;
         if (isSignal("BO", signal))
                 CPUSTATE.Bus = CPUSTATE.B;
         if (isSignal("CO", signal))
                 CPUSTATE.Bus = CPUSTATE.C;
+        if (isSignal("XO", signal))
+                CPUSTATE.Bus = CPUSTATE.X;
         if (isSignal("RO", signal))
         {
                 CPUSTATE.Bus = CPUSTATE.RAM[CPUSTATE.MR];
@@ -228,21 +265,13 @@ void executeMicroFalling(Word signal)
                 {
                         CPUSTATE.Bus = recvByte();
                 }
-                else if(CPUSTATE.MR == 0x2001)
-                {
-                    CPUSTATE.Bus=CPUSTATE.BP;
-                }
         }
+        if(isSignal("NOFLG",signal))
+            enableFlags=false;
         if (isSignal("POH", signal))
                 CPUSTATE.Bus = ((CPUSTATE.PC >> 8) & 0xFF);
         if (isSignal("POL", signal))
                 CPUSTATE.Bus = (CPUSTATE.PC & 0xFF);
-
-        if (isSignal("IGB", signal))
-        {
-                CPUSTATE.Bus = (CPUSTATE.A + 1);
-                setFlags();
-        }
 
         if (isSignal("ADD", signal))
         {
@@ -296,21 +325,26 @@ int main(int argc, char **argv)
         }*/
 
         unsigned char *rMCode1 = reinterpret_cast<unsigned char *>(malloc(16384)),
-                      *rMCode2 = reinterpret_cast<unsigned char *>(malloc(16384));
-        std::ifstream microcodeR1, microcodeR2;
+                      *rMCode2 = reinterpret_cast<unsigned char *>(malloc(16384)),
+                      *rMCode3 = reinterpret_cast<unsigned char *>(malloc(16384));
+        
+        std::ifstream microcodeR1, microcodeR2, microcodeR3;
         microcodeR1.open("microcode1.bin", std::ios::binary);
         microcodeR2.open("microcode2.bin", std::ios::binary);
+        microcodeR3.open("microcode3.bin", std::ios::binary);
 
         microcodeR1.read(reinterpret_cast<char *>(rMCode1), 16384);
         microcodeR2.read(reinterpret_cast<char *>(rMCode2), 16384);
+        microcodeR3.read(reinterpret_cast<char *>(rMCode3), 16384);
         int i = 0, j;
 
         for (; i < 16384; i++)
         {
-                mCode[i] = rMCode1[i] | (rMCode2[i] << 8);
+                mCode[i] = rMCode1[i] | (rMCode2[i] << 8) | (rMCode3[i]<<16);
         }
         free(rMCode1);
         free(rMCode2);
+        free(rMCode3);
 
         std::ifstream file("RAM.bin", std::ios::binary);
         file.seekg(0, file.end);
@@ -328,35 +362,45 @@ int main(int argc, char **argv)
         CPUSTATE.MR = 0;
         CPUSTATE.IR = 0;
         CPUSTATE.SP = 0xff00;
-        CPUSTATE.BP = 0xff00;
+        CPUSTATE.FP = 0xff00;
         for (; 1;)
         {
+                int c;
                 for (CPUSTATE.MicroStep = 0; CPUSTATE.MicroStep < 16; CPUSTATE.MicroStep++)
                 {
-#ifdef DEBUG
-                        getchar();
+#ifdef DEBUG0
+
+                        c=getch();
                         // if(CPUSTATE.IR==118)
                         showMicro(mCode[CPUSTATE.IR * 16 + CPUSTATE.MicroStep + CPUSTATE.CarryF * 4096 + CPUSTATE.ZeroF * 8192]);
 #endif
                         executeMicroFalling(mCode[CPUSTATE.IR * 16 + CPUSTATE.MicroStep + CPUSTATE.CarryF * 4096 + CPUSTATE.ZeroF * 8192]);
                         executeMicroRising(mCode[CPUSTATE.IR * 16 + CPUSTATE.MicroStep + CPUSTATE.CarryF * 4096 + CPUSTATE.ZeroF * 8192]);
                 }
-#ifdef DEBUG
-                std::cout <<std::hex<< " IR:" << static_cast<int>(CPUSTATE.IR) << " A:" << static_cast<int>(CPUSTATE.A)
+#ifdef DEBUG1
+                std::cout <<std::hex<< "\nIR:" << static_cast<int>(CPUSTATE.IR) << " A:" << static_cast<int>(CPUSTATE.A)
                           << " B:" << static_cast<int>(CPUSTATE.B) << " Z:" << static_cast<int>(CPUSTATE.ZeroF)
                           << " C:" << static_cast<int>(CPUSTATE.CarryF) << " PC:" << static_cast<int>(CPUSTATE.PC)
-                          << " SP:" << static_cast<int>(CPUSTATE.SP) <<"\n";
+                          << " SP:" << static_cast<int>(CPUSTATE.SP) <<"\n"
+                          << " FP:" << static_cast<int>(CPUSTATE.FP) <<"\n";
                         if(CPUSTATE.SP>=0xfff0){
                             int s=CPUSTATE.SP-0xfff0;
                             for(i=0;i<s;i++)std::cout<<"   ";
-                            std::cout<<"#\n";
+                            std::cout<<"P\n";
+                        }
+                        if(CPUSTATE.FP>=0xfff0){
+                            int s=CPUSTATE.FP-0xfff0;
+                            for(i=0;i<s;i++)std::cout<<"   ";
+                            std::cout<<"F\n";
                         }
                 for(i=0;i<16;i++){
                         std::cout<<std::hex<<std::setw(2)<<std::setfill('0')<<static_cast<int>(CPUSTATE.RAM[i+0xfff0])<<" ";
                 }
                 std::cout<<std::dec<< "\n";
+               
+
 #endif
-                if(CPUSTATE.IR==0x94)
+                if(CPUSTATE.IR==0x96||c==27)
                     break;
         }
 }
